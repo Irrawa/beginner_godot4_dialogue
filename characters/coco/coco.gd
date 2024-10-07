8f40cf2d9f6051e3c220617aab04731e4e568e7e
Irrawa <Irrawa>
2024-10-08 00:30:52 +0800

    try multiple functions

--- characters/coco/coco.gd
+++ characters/coco/coco.gd
@@ -8,12 +8,20 @@ const SPEED = 50
 
 var input_vector: Vector2 = Vector2.ZERO
 
+var attr = 10
+
+const Balloon = preload("res://dialogues/balloon.tscn")
+
+var summoned_creature_name = "未音"
 
 func _ready() -> void:
 	animation_tree.active = true
 
 
 func _unhandled_input(_event: InputEvent) -> void:
+	test_speak()
+	# test_modified_speek()
+	
 	var x_axis: float = Input.get_axis("ui_left", "ui_right")
 	var y_axis: float = Input.get_axis("ui_up", "ui_down")
 	if x_axis:
@@ -22,6 +30,19 @@ func _unhandled_input(_event: InputEvent) -> void:
 		input_vector = y_axis * Vector2.DOWN
 	else:
 		input_vector = Vector2.ZERO
+
+func test_speak() -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		DialogueManager.show_dialogue_balloon(load("res://dialogues/another_test_dialogue.dialogue"), "start")
+
+func test_modified_speek() -> void:
+	var balloon: Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(load("res://dialogues/test_dialogue.dialogue"), "this_is_a_node_title")
+
+func do_something() -> void:
+	print("Coco do something...Like ignite a bomb.")
+	print(attr)
 
 
 func _physics_process(_delta: float) -> void:
---
+++ globals/global_data.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var attr: int = 10
+
+var irrawa_intimacy = 80
+var lord_intimacy = 70
+
+func show_attr() -> void:
+	print(attr)
+
+func set_attr(new_attr: int) -> void:
+	attr = new_attr
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
